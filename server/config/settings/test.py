26df5cd063a6b8d5bdb5bcd307fdedaf6ca77b1f
Tosmak <tosmak16@gmail.com>
2019-07-01 20:57:00 +0100

    Add settings for different environments

---
+++ server/config/settings/test.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('DB_NAME_TEST'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'PORT': getenv('DB_PORT'),
+        'HOST': getenv('DB_HOST'),
+    }
+}
