ef5656032bb679cfd73130747033d5068e0981ec
Oluwatosin Akinola <30419014+tosmak16@users.noreply.github.com>
2019-07-01 21:19:18 +0100

    Add audit logs views and tests (#8)

---
+++ server/favorites/tests/test_audit_logs.py
@@ -0,0 +1,18 @@
+from django.urls import reverse
+from rest_framework import status
+from django.urls import reverse
+
+from .test_base import TestBase
+
+
+class AuditLogsTests(TestBase):
+
+    url = reverse('audit_logs')
+
+    def test_get_favorite_success(self):
+        """
+        Get audit logs success
+        """
+
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
