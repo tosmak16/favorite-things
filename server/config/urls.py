c0e808ff4e383bd6f9e50b8a1639768bd1946093
Oluwatosin Akinola <30419014+tosmak16@users.noreply.github.com>
2019-07-01 23:06:11 +0100

    Add api documentation (#10)

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from django.conf.urls import url
 from rest_framework.authtoken import views
+from rest_framework.documentation import include_docs_urls
 
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url('^api/', include('favorites.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'api/docs/', include_docs_urls(title='Favorite things API')),
 ]
