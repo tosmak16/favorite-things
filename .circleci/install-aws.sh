9af23edf6eb9575024f824a729a3548a2d7e0a22
Tosmak <tosmak16@gmail.com>
2019-07-03 04:59:07 +0100

    Initial Client setup

---
+++ .circleci/install-aws.sh
@@ -0,0 +1,6 @@
+if ! [ -x "$(command -v pip)" ]; then
+    sudo apt-get update && sudo apt-get install -qq -y python-pip libpython-dev
+    curl -O https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
+fi
+sudo pip install wheel
+sudo pip install awscli==1.11.18
